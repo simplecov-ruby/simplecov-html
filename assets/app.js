7d4134997875e8f3b3ee056254faebeea7d8c14c
Christoph Olszowka <christoph@olszowka.de>
2010-08-25 01:45:16 +0200

    Store open tab in url so on reload, the last active tab can be reopened via JS

--- assets/app.js
+++ assets/app.js
@@ -12,35 +12,46 @@ $(document).ready(function() {
 			null
 		]
   });
+  $('.source_table tbody tr:odd').addClass('odd');
+  $('.source_table tbody tr:even').addClass('even');
   
   $("a.src_link").fancybox({
 		'hideOnContentClick': true
 	});
 	
-  $('.source_table tbody tr:odd').addClass('odd');
-  $('.source_table tbody tr:even').addClass('even');
-  
+	// Hide src files and file list container
   $('.source_files').hide();
   $('.file_list_container').hide();
   
+  // Add tabs based upon existing file_list_containers
   $('.file_list_container h2').each(function(){
-    $('.group_tabs').append('<li><a href="' + $(this).attr('id') + '">' + $(this).html() + '</a></li>');
+    $('.group_tabs').append('<li><a href="#' + $(this).parent().attr('id') + '">' + $(this).html() + '</a></li>');
   });
 
+  $('.group_tabs a').each( function() {
+    $(this).addClass($(this).attr('href').replace('#', ''));
+  });
+  
   $('.group_tabs a').live('focus', function() {
     $(this).blur();
   });
+  
   $('.group_tabs a').live('click', function(){
     if (!$(this).parent().hasClass('active')) {
       $('.group_tabs a').parent().removeClass('active');
       $(this).parent().addClass('active');
       $('.file_list_container').hide();
-      $(".file_list_container h2#" + $(this).attr('href')).parent().show();
+      $(".file_list_container" + $(this).attr('href')).show();
+      window.location.href = window.location.href.split('#')[0] + $(this).attr('href').replace('#', '#_');
     };
     return false;
   });
   
-  $('.group_tabs a:first').click();
+  if (jQuery.url.attr('anchor')) {
+    $('.group_tabs a.'+jQuery.url.attr('anchor').replace('_', '')).click();
+  } else {
+    $('.group_tabs a:first').click();
+  };
   
   $("abbr.timeago").timeago();
   $('#loading').fadeOut();
