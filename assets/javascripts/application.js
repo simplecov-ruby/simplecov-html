354622aac86aa2ccc3bf291c437ba6a8680b9e78
Rajesh Konda <rkonda@ooyala.com>
2013-05-02 00:22:02 -0700

    JS used for group_tabs repeated for report_tabs

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -67,6 +67,7 @@ $(document).ready(function() {
 	// Hide src files and file list container after load
   $('.source_files').hide();
   $('.file_list_container').hide();
+  $('.report_container').hide();
 
   // Add tabs based upon existing file_list_containers
   $('.file_list_container h2').each(function(){
@@ -77,18 +78,35 @@ $(document).ready(function() {
     $('.group_tabs').append('<li><a href="#' + container_id + '">' + group_name + ' ('+ covered_percent +')</a></li>');
   });
 
+  // Add tabs based upon existing report_containers
+  $('.report_container h2').each(function(){
+    var container_id = $(this).parent().attr('id');
+    var group_name = $(this).find('.group_name').first().html();
+
+    $('.report_tabs').append('<li><a href="#' + container_id + '">' + group_name + '</a></li>');
+  });
+
   $('.group_tabs a').each( function() {
     $(this).addClass($(this).attr('href').replace('#', ''));
   });
 
-  // Make sure tabs don't get ugly focus borders when active
+  $('.report_tabs a').each( function() {
+    $(this).addClass($(this).attr('href').replace('#', ''));
+  });
+
+    // Make sure tabs don't get ugly focus borders when active
   $('.group_tabs a').live('focus', function() { $(this).blur(); });
+  $('.report_tabs a').live('focus', function() { $(this).blur(); });
 
   var favicon_path = $('link[rel="shortcut icon"]').attr('href');
   $('.group_tabs a').live('click', function(){
     if (!$(this).parent().hasClass('active')) {
       $('.group_tabs a').parent().removeClass('active');
       $(this).parent().addClass('active');
+
+      $('.report_container').hide();
+      $('.report_tabs a').parent().removeClass('active');
+
       $('.file_list_container').hide();
       $(".file_list_container" + $(this).attr('href')).show();
       window.location.href = window.location.href.split('#')[0] + $(this).attr('href').replace('#', '#_');
@@ -101,8 +119,29 @@ $(document).ready(function() {
     return false;
   });
 
+  $('.report_tabs a').live('click', function(){
+    if (!$(this).parent().hasClass('active')) {
+      $('.report_tabs a').parent().removeClass('active');
+      $(this).parent().addClass('active');
+
+      $('.file_list_container').hide();
+      $('.group_tabs a').parent().removeClass('active');
+
+      $('.report_container').hide();
+      $(".report_container" + $(this).attr('href')).show();
+      window.location.href = window.location.href.split('#')[0] + $(this).attr('href').replace('#', '#_');
+
+      // Force favicon reload - otherwise the location change containing anchor would drop the favicon...
+      // Works only on firefox, but still... - Anyone know a better solution to force favicon on local file?
+      $('link[rel="shortcut icon"]').remove();
+      $('head').append('<link rel="shortcut icon" type="image/png" href="'+ favicon_path +'" />');
+    };
+    return false;
+  });
+
   if (jQuery.url.attr('anchor')) {
     $('.group_tabs a.'+jQuery.url.attr('anchor').replace('_', '')).click();
+    $('.report_tabs a.'+jQuery.url.attr('anchor').replace('_', '')).click();
   } else {
     $('.group_tabs a:first').click();
   };
